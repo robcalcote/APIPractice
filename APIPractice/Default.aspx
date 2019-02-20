fb413ff53a4b71500d3c5059d6a88072e61ce340
Rob <robcalcote@gmail.com>
2019-02-20 13:11:23 -0700

    Added code from postman and installed RestSharp

---
+++ APIPractice/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="APIPractice.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Bike Buyers Prediction Calculator</title>
+    <style>
+        body {font-family: Arial; font-size: 12px;}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Rob Calcote IS 415 Bike Buyers Prediction Calculator</h1>
+        </div>
+        <asp:Label ID="lblResults" runat="server" Text="{Results Show here}"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ APIPractice/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RestSharp;
+
+namespace APIPractice
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // This is all taken from PostMan
+            var client = new RestClient("https://ussouthcentral.services.azureml.net/workspaces/00bdc3fc373e4c65bc3ca23c91c40eea/services/ca581ae89ad44095874e1dcb3d91bb51/execute?api-version=2.0&details=true");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Postman-Token", "b22b4d05-92c9-4640-a946-dd7b7a55d4f4");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer fa1Pe9yLdV9+o/NziaNSvCBn4M33GbILgPotrgO4D1c9VqMbBKRiOTJnZCc+30x4hPoBRqOXUytWhb8I8i4rdg==");
+            request.AddParameter("undefined", "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"Marital Status\",\r\n        \"Gender\",\r\n        \"Income\",\r\n        \"Children\",\r\n        \"Cars\",\r\n        \"Age\",\r\n        \"PurchaseBikeNumeric\",\r\n        \"Education\",\r\n        \"Occupation\",\r\n        \"Home Owner\",\r\n        \"Commute Distance\",\r\n        \"Region\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"Married\",\r\n          \"Female\",\r\n          \"100000\",\r\n          \"2\",\r\n          \"2\",\r\n          \"38\",\r\n          \"0\",\r\n          \"Graduate\",\r\n          \"Management\",\r\n          \"Yes\",\r\n          \"0-1 Miles\",\r\n          \"Europe\"\r\n        ]\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            // PostMan code stops here
+        }
+    }
+}
